d7c5362b3d4130fb466f1b851fdc277b8521e7c2
Monika Rehakova <xrehako1@fi.muni.cz>
2024-06-09 16:08:34 +0200

    feat: changed color palette

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,32 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import {CssBaseline} from "@mui/material";
 
 function App() {
     const theme = createTheme({
         palette: {
             primary: {
-                main: '#1976d2',
+                main: '#4CAF50'
             },
             secondary: {
-                main: '#DDDDDD',
-            }
+                main: '#8D6E63',
+            },
+            background: {
+                default: '#FFFFFF',
+                paper: '#F5F5F5',
+            },
+            text: {
+                primary: '#000000',
+                secondary: '#FFFFFF',
+            },
         },
     });
 
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
+
             <RouterProvider router={router} />
         </ThemeProvider>
     );
--- app/src/components/MainLayout.tsx
+++ app/src/components/MainLayout.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { useTheme } from '@mui/material/styles';
 
 const MainLayout: React.FC = () => {
+    const theme = useTheme();
+
     return (
         <>
             <Navbar />
-            <div style={{ marginTop: '64px', padding: '16px' }}>
+            <div style={{
+                marginTop: '64px',
+                padding: '16px',
+                backgroundColor: theme.palette.background.default,
+                color: theme.palette.text.primary
+            }}>
                 <Outlet />
             </div>
         </>
