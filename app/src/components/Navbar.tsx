4b8be5925efb58c1ba95aa78c9c9b79621a95aa1
Martina O <martina22ondrejickova@gmail.com>
2024-06-18 20:17:47 +0200

    Navbar Container

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import { Container } from "@mui/material";
 
 const Navbar: React.FC = () => {
-    return (
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography variant="h6" component={Link} to="/" style={{ textDecoration: 'none', color: '#355e3b', fontFamily: "Jost" }}>
-                    PlantMingle
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Container>
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#355e3b",
+              fontFamily: "Jost",
+            }}
+          >
+            PlantMingle
+          </Typography>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
